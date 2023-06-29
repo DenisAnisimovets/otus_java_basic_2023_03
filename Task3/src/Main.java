e34f298ad81d7b697668342d38351eb4dfbc47db
Anisimovets <anisimovets@mail.ru>
2023-06-29 18:06:14 +0300

    homework03

--- Task3/src/Main.java
+++ Task3/src/Main.java
@@ -6,13 +6,13 @@ public class Main {
         int wrightAnswers = 0;
         String [] questions = new String[] {
                 "2 + 2 = ?     1) - 4, 2) - 5, 3) - 9",
-                "2 * 3 = ?     1) - 4, 3) - 6, 3) - 9",
-                "3 * 3 = ?     1) - 4, 3) - 6, 3) - 9"
+                "2 * 3 = ?     1) - 4, 2) - 1, 3) - 6",
+                "3 * 3 = ?     1) - 4, 2) - 9, 3) - 5"
         };
         String  [] answers = new String[] {
                 "1",
-                "2",
-                "3"
+                "3",
+                "2"
         };
 
         for(int i = 0; i < questions.length ; i++) {
