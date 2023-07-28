b8e0c274a0819fa4b92d8928c55832254d39f432
Anisimovets <anisimovets@mail.ru>
2023-07-29 00:36:01 +0300

    homework05-refactor

--- Task5/src/Task5.java
+++ Task5/src/Task5.java
@@ -1,16 +1,20 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 
 public class Task5 {
     public static void main(String[] args) {
-        int size = 10000;
-        int[] array1 = new int[size];
-        int[] array2 = new int[size];
+        int size = 100000;
+        List<Integer> array1 = new ArrayList<>();
+        List<Integer> array2 = new ArrayList<>();
         boolean isEqual = true;
         Random random = new Random(10000);
         for (int i = 0; i < size; i++) {
-            array1[i] = random.nextInt();
-            array2[i] = array1[i];
+            array1.add(random.nextInt());
+            array2.add(array1.get(i));
         }
+
 
         long startTimePrimitives = System.currentTimeMillis();
         bubbleSort(array1);
@@ -18,14 +22,14 @@ public class Task5 {
         System.out.println("Bubble sort: " + (endTimePrimitives - startTimePrimitives) + " milliseconds");
 
         startTimePrimitives = System.currentTimeMillis();
-        bubbleSort(array2);
+        Collections.sort(array2);
         endTimePrimitives = System.currentTimeMillis();
         System.out.println("Collection sort: " + (endTimePrimitives - startTimePrimitives) + " milliseconds");
 
         for (int i = 0; i < size; i++) {
-            if(array1[i] != array2[i]) {
+            if(!array1.get(i).equals(array2.get(i))) {
                 isEqual = false;
-                System.out.println(i + " " + array1[i] + " " + array2[i]);
+                System.out.println(i + " " + array1.get(i) + " " + array2.get(i));
             }
         }
 
@@ -36,14 +40,15 @@ public class Task5 {
         }
     }
 
-    public static void bubbleSort(int[] arr) {
-        int n = arr.length;
-        for (int i = 0; i < n; i++) {
+    public static void bubbleSort(List<Integer> list) {
+        int n = list.size();
+        for (int i = 0; i < n ; i++) {
             for (int j = 0; j < n - i - 1; j++) {
-                if(arr[j] >= arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
+                if(list.get(j) > list.get(j + 1)) {
+                    // Меняем местами элементы
+                    int temp = list.get(j);
+                    list.set(j, list.get(j + 1));
+                    list.set(j + 1, temp);
                 }
             }
         }
